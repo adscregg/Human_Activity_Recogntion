80962e86a14f6fc49337cb815c6db8d3addee019
Anthony Scregg (ads1e20) <ads1e20@soton.ac.uk>
2021-07-03 16:40:56 +0100

    add all model training blocks main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from torchvision.models import resnet101
+from torchvision.models import resnet101, squeezenet1_1, densenet121, shufflenet_v2_x1_0
 from torch.utils.data import DataLoader
 from torch.nn import CrossEntropyLoss
 from torch import optim
@@ -15,6 +15,7 @@ from identity import Identity
 BATCH_SIZE = 64
 DATA_DIR = './data/NTU_RGB+D/transformed_images'
 WEIGHTS_PATH = './weights/'
+SUMMARIES_PATH = './model_summaries/'
 DEVICE = 'cuda'
 TRAIN_SUBJECT_IDS = [1, 2, 4, 5, 8, 9, 13, 14, 15, 16, 17, 18, 19, 25, 27, 28, 31, 34, 35, 38]
 TEST_SUBJECT_IDS = [3, 6, 7, 10, 11, 12, 20, 21, 22, 23, 24, 26, 29, 30, 32, 33, 38, 37, 39, 40]
@@ -66,23 +67,316 @@ scattering_testloader_all_subs = DataLoader(scattering_test, batch_size = BATCH_
 
 # ============================ TRAINING ===============================================================
 
+
+# ============== ResNet =========================
+
+# === 5 Subjects ===
+
+ResNet = resnet101(pretrained = True) # define model architecture
+ResNet.fc = Identity() # remove final classification layer so multi-layer stucture can be implemented
+MS_CNN = MultiScalePretrained(ResNet).to(DEVICE) # create multi-layer architecture model
+weights_file = 'resnet_5_subs.pth' # file name of the weights of the model
+summary_file = 'resnet_5_subs.json' # file name of the model summary
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001) # define the optimiser
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser) # define the scheduler (optional, will change learning rate during training if supplied to runModel)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_5_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True) # start training loop
+MS_CNN_class.create_model_summary('ResNet') # create the model summary
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file) # save model weights
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file) # save model summary
+
+
+# === 10 Subjects ===
+
 ResNet = resnet101(pretrained = True)
 ResNet.fc = Identity()
-resnet_multiscale = MultiScalePretrained(ResNet).to(DEVICE)
-weights_file = 'example.pth'
-optimiser = optim.Adam(resnet_multiscale.parameters(), lr = 0.001)
+MS_CNN = MultiScalePretrained(ResNet).to(DEVICE)
+weights_file = 'resnet_10_subs.pth'
+summary_file = 'resnet_10_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
-resnet_multiscale_class = runModel(resnet_multiscale, DEVICE, optimiser, loss_fn, images_trainloader_all_subs, images_testloader_all_subs, scheduler)
-# resnet_multiscale_class.train(epochs = 50, validate = True)
-# resnet_multiscale_class.save_model(WEIGHTS_PATH+weights_file)
-# print(resnet_multiscale_class.test_accuracy)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_10_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ResNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 15 Subjects ===
+
+ResNet = resnet101(pretrained = True)
+ResNet.fc = Identity()
+MS_CNN = MultiScalePretrained(ResNet).to(DEVICE)
+weights_file = 'resnet_15_subs.pth'
+summary_file = 'resnet_15_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_15_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ResNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === All Subjects ===
+
+ResNet = resnet101(pretrained = True)
+ResNet.fc = Identity()
+MS_CNN = MultiScalePretrained(ResNet).to(DEVICE)
+weights_file = 'resnet_all_subs.pth'
+summary_file = 'resnet_all_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_all_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ResNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# ============== ShuffleNet =========================
+
+# === 5 Subjects ===
+
+ShuffleNet = shufflenet_v2_x1_0(pretrained = True)
+ShuffleNet.fc = Identity()
+MS_CNN = MultiScalePretrained(ShuffleNet).to(DEVICE)
+weights_file = 'shufflenet_5_subs.pth'
+summary_file = 'shufflenet_5_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_5_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ShuffleNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 10 Subjects ===
+
+ShuffleNet = shufflenet_v2_x1_0(pretrained = True)
+ShuffleNet.fc = Identity()
+MS_CNN = MultiScalePretrained(ShuffleNet).to(DEVICE)
+weights_file = 'shufflenet_10_subs.pth'
+summary_file = 'shufflenet_10_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_10_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ShuffleNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 15 Subjects ===
+
+ShuffleNet = shufflenet_v2_x1_0(pretrained = True)
+ShuffleNet.fc = Identity()
+MS_CNN = MultiScalePretrained(ShuffleNet).to(DEVICE)
+weights_file = 'shufflenet_15_subs.pth'
+summary_file = 'shufflenet_15_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_15_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ShuffleNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === All Subjects ===
+
+ShuffleNet = shufflenet_v2_x1_0(pretrained = True)
+ShuffleNet.fc = Identity()
+MS_CNN = MultiScalePretrained(ShuffleNet).to(DEVICE)
+weights_file = 'shufflenet_all_subs.pth'
+summary_file = 'shufflenet_all_subs.json'
+optimiser = optim.Adam(MS_CNN.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+MS_CNN_class = runModel(MS_CNN, DEVICE, optimiser, loss_fn, images_trainloader_all_subs, images_testloader_all_subs, scheduler)
+# MS_CNN_class.train(epochs = 50, validate = True)
+MS_CNN_class.create_model_summary('ShuffleNet')
+MS_CNN_class.save_model(WEIGHTS_PATH + weights_file)
+MS_CNN_class.save_model_summary(SUMMARIES_PATH + summary_file)
 
 
 
-ScatNet = ScatteringModel(in_size_J4_L8, [256, 512, 512, 256]).to(DEVICE)
-weights_file = 'example.pth'
+
+# =========== Linear Scattering ====================
+
+# === 5 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8).to(DEVICE)
+weights_file = 'lin_scattering_5_subs.pth'
+summary_file = 'lin_scattering_5_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_10_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Lin Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 10 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8).to(DEVICE)
+weights_file = 'lin_scattering_10_subs.pth'
+summary_file = 'lin_scattering_10_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_10_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Lin Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 15 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8).to(DEVICE)
+weights_file = 'lin_scattering_15_subs.pth'
+summary_file = 'lin_scattering_15_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_15_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Lin Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === All Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8).to(DEVICE)
+weights_file = 'lin_scattering_all_subs.pth'
+summary_file = 'lin_scattering_all_subs.json'
 optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
 ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_all_subs, scattering_testloader_all_subs, scheduler)
-ScatNet_class.train(epochs = 500, validate = False)
-# ScatNet_class.save_model(WEIGHTS_PATH+weights_file)
+ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Lin Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+
+# ============ Shallow Scattering =====================
+
+layers = [256, 512]
+
+# === 5 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'shallow_scattering_5_subs.pth'
+summary_file = 'shallow_scattering_5_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_10_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Shallow Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 10 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'shallow_scattering_10_subs.pth'
+summary_file = 'shallow_scattering_10_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_10_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Shallow Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 15 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'shallow_scattering_15_subs.pth'
+summary_file = 'shallow_scattering_15_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_15_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Shallow Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === All Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'shallow_scattering_all_subs.pth'
+summary_file = 'shallow_scattering_all_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_all_subs, scattering_testloader_all_subs, scheduler)
+ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Shallow Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+
+# ============ Deep Scattering =====================
+
+layers = [256, 512, 512, 256]
+
+# === 5 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'deep_scattering_5_subs.pth'
+summary_file = 'deep_scattering_5_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_10_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Deep Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 10 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'deep_scattering_10_subs.pth'
+summary_file = 'deep_scattering_10_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_10_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Deep Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === 15 Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'deep_scattering_15_subs.pth'
+summary_file = 'deep_scattering_15_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_15_subs, scattering_testloader_all_subs, scheduler)
+# ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Deep Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
+
+
+# === All Subjects ===
+
+ScatNet = ScatteringModel(in_size_J4_L8, layers).to(DEVICE)
+weights_file = 'deep_scattering_all_subs.pth'
+summary_file = 'deep_scattering_all_subs.json'
+optimiser = optim.Adam(ScatNet.parameters(), lr = 0.001)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser)
+ScatNet_class = runModel(ScatNet, DEVICE, optimiser, loss_fn, scattering_trainloader_all_subs, scattering_testloader_all_subs, scheduler)
+ScatNet_class.train(epochs = 50, validate = True)
+ScatNet_class.create_model_summary('Deep Scattering')
+ScatNet_class.save_model(WEIGHTS_PATH + weights_file)
+ScatNet_class.save_model_summary(SUMMARIES_PATH + summary_file)
