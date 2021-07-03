9a64b508e7b2a1fa22d35348a668817a75667d5a
Anthony Scregg (ads1e20) <ads1e20@soton.ac.uk>
2021-07-03 11:33:23 +0100

    added plot_curves func to visualise.py

--- visualise.py
+++ visualise.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import json
 import os
 
 
 def visualise(data_dir, subjects = None, save_name = None):
     if subjects is None:
-        subjects = ["P" + f"{a:03}" for a in range(1,6)]
+        subjects = ["P" + f"{a:03}" for a in range(1,41)]
     files = os.listdir(data_dir)
     files_to_vis = filter(lambda x: x[8:12] in subjects, files)
     dict_counter = dict()
@@ -18,8 +19,24 @@ def visualise(data_dir, subjects = None, save_name = None):
     # plt.xticks([])
     plt.show()
 
+def plot_curves(summary_dir, files, key, title, xlabel, ylabel):
+    fig, ax = plt.subplots()
+    for file in files:
+        with open(summary_dir + file) as f:
+            summary = json.load(f)
+        data = summary[key]
+        ax.plot(data, label = summary['name'])
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend()
+    return fig, ax
+
 if __name__ == '__main__':
 
-    data_dir = './data/NTU_RGB+D/nturgbd_skeletons_s001_to_s017/nturgb+d_skeletons'
+    data_dir = './data/NTU_RGB+D/transformed_images'
+    summary_dir = './model_summaries/'
 
-    visualise(data_dir)
+    fig, ax = plot_curves(summary_dir, ['test.json'], 'train accuracy history','TEST', 'EPOCHS', 'ACCURACY')
+    fig1, ax1 = plot_curves(summary_dir, ['test.json', 'test1.json'], 'train accuracy history','HAPPY', 'EPOCHS', 'ACCURACY')
+    plt.show()
