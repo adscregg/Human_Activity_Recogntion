fc82f5660d8fee1b0b2fc1821408fcc23619f906
Anthony Scregg (ads1e20) <ads1e20@soton.ac.uk>
2021-07-03 16:41:42 +0100

    added docstring visualise.py

--- visualise.py
+++ visualise.py
@@ -20,6 +20,34 @@ def visualise(data_dir, subjects = None, save_name = None):
     plt.show()
 
 def plot_curves(summary_dir, files, key, title, xlabel, ylabel):
+    """
+    Plot curves associated with the training of models, can plot, the learning rate, loss and accuracy curves for any model
+
+    Parameters
+    ----------
+    summary_dir: str
+        File path to the directory where the model summary .json files are stored
+
+    files: list or iterable
+        Summary files that contain the values to be plotted
+
+    key: str
+        The metric that will be plotted on the graph
+
+    title: str
+        Title of the figure
+
+    xlabel: str
+        Label of the x-axis
+
+    ylabel: str
+        Label of the y-axis
+
+    Returns
+    -------
+    fig, ax:
+        matplotlib figure object and matplotlib Axis object
+    """
     fig, ax = plt.subplots()
     for file in files:
         with open(summary_dir + file) as f:
@@ -37,6 +65,11 @@ if __name__ == '__main__':
     data_dir = './data/NTU_RGB+D/transformed_images'
     summary_dir = './model_summaries/'
 
-    fig, ax = plot_curves(summary_dir, ['test.json'], 'train accuracy history','TEST', 'EPOCHS', 'ACCURACY')
-    fig1, ax1 = plot_curves(summary_dir, ['test.json', 'test1.json'], 'train accuracy history','HAPPY', 'EPOCHS', 'ACCURACY')
+    save_fig_dir = './figures/'
+
+    fig, ax = plot_curves(summary_dir, ['test.json', 'test1.json'], 'lr history','lr', 'EPOCHS', 'ACCURACY')
+    fig1, ax1 = plot_curves(summary_dir, ['test.json', 'test1.json'], 'test accuracy history','test', 'EPOCHS', 'ACCURACY')
     plt.show()
+
+    fig.savefig(save_fig_dir + 'test1.pdf')
+    fig1.savefig(save_fig_dir + 'happy1.pdf')
